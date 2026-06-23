3453718f2e8bb8882dbf77ecabde0b75f9f087fb
PawanBhandari03 <pawansinghb07@gmail.com>
2026-06-23 08:43:42 +0530

    Added Relationship (one and many)

--- src/main/java/com/pawan/blog/domain/entities/Post.java
+++ src/main/java/com/pawan/blog/domain/entities/Post.java
@@ -4,7 +4,9 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -36,6 +38,18 @@ public class Post {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "author_id",nullable = false)
     private User author;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id",nullable = false)
+    private User category;
+
+    @ManyToMany
+    @JoinTable(
+            name = "post_tags",
+            joinColumns = @JoinColumn(name="post_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )
+    private Set<Tag> tags = new HashSet<>();
 
     @Column(nullable = false)
     private LocalDateTime createdAt;
